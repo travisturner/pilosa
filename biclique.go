c53dda2f3def47029ad53309964dd21e52fbaa74
jaffee <jaffee@umbel.com>
2016-03-01 08:40:13 -0600

    remove PairSlice, implement MBEA (not hooked up)

--- biclique.go
+++ biclique.go
@@ -2,8 +2,6 @@ package pilosa
 
 import "fmt"
 
-type PairSlice []BitmapPair
-
 type Biclique struct {
 	Tiles []uint64
 	Count uint64 // number of profiles
@@ -39,8 +37,8 @@ func (f *Fragment) MaxBiclique(n int) []Biclique {
 
 func maxBiclique(topPairs []BitmapPair) []Biclique {
 	// generate every permutation of topPairs
-	pairChan := make(chan PairSlice, 10)
-	ps := PairSlice(topPairs)
+	pairChan := make(chan []BitmapPair, 10)
+	ps := []BitmapPair(topPairs)
 	go generateCombinations(ps, pairChan)
 	var minCount uint64 = 1
 
@@ -67,7 +65,69 @@ func maxBiclique(topPairs []BitmapPair) []Biclique {
 	return results
 }
 
-func getTileIDs(pairs PairSlice) []uint64 {
+func bicliqueFind(G []BitmapPair, L *Bitmap, R []BitmapPair, P []BitmapPair, Q []BitmapPair, results chan *Bitmap) {
+	// G is topPairs
+	// L should start with all bits set (L == U)
+	// R starts empty
+	// P starts as topPairs (all tiles are candidates)
+	// Q starts empty
+
+	for len(P) > 0 {
+		// P ← P\{x};
+		x := P[0]
+		P = P[1:]
+
+		// R ← R ∪ {x};
+		newR := append(R, x)
+
+		//  L' ← {u ∈ L | (u, x) ∈ E(G)};
+		newL := L.Clone()
+		newL = newL.Intersect(x.Bitmap)
+		newLcnt := newL.BitCount()
+
+		// P' ← ∅; Q' ← ∅;
+		newP := []BitmapPair{}
+		newQ := []BitmapPair{}
+
+		// Check maximality.
+		isMaximal := true
+		for _, v := range Q {
+			// get the neighbors of v in L'
+			neighbors := v.Bitmap.Intersect(newL)
+			ncnt := neighbors.BitCount()
+			// Observation 4: end of branch
+			if ncnt == newLcnt {
+				isMaximal = false
+				break
+			} else if ncnt > 0 {
+				newQ = append(newQ, v)
+			}
+		}
+
+		if isMaximal {
+			for _, v := range P {
+				// get the neighbors of v in L'
+				neighbors := v.Bitmap.Intersect(newL)
+				ncnt := neighbors.BitCount()
+				// Observation 3: expand to maximal
+				if ncnt == newLcnt {
+					newR = append(newR, v)
+				} else if ncnt > 0 {
+					// keep vertice adjacent to some vertex in newL
+					newP = append(newP, v)
+				}
+			}
+			// report newR as maximal bicliqued
+			results <- newL
+			if len(newP) > 0 {
+				bicliqueFind(G, newL, newR, newP, newQ, results)
+			}
+		}
+		Q = append(Q, x)
+	}
+}
+
+func getTileIDs(pairs []BitmapPair) []uint64 {
 	tileIDs := make([]uint64, len(pairs))
 	for i := 0; i < len(pairs); i++ {
 		tileIDs[i] = pairs[i].ID
@@ -75,12 +135,12 @@ func getTileIDs(pairs PairSlice) []uint64 {
 	return tileIDs
 }
 
-func generateCombinations(pairs PairSlice, pairChan chan<- PairSlice) {
+func generateCombinations(pairs []BitmapPair, pairChan chan<- []BitmapPair) {
 	gcombs(pairs, pairChan)
 	close(pairChan)
 }
 
-func gcombs(pairs PairSlice, pairChan chan<- PairSlice) {
+func gcombs(pairs []BitmapPair, pairChan chan<- []BitmapPair) {
 	fmt.Println("gcombs, send to pairChan ", pairs)
 
 	pairChan <- pairs
@@ -89,7 +149,7 @@ func gcombs(pairs PairSlice, pairChan chan<- PairSlice) {
 		return
 	}
 	for i := 0; i < len(pairs); i++ {
-		pairscopy := make(PairSlice, len(pairs))
+		pairscopy := make([]BitmapPair, len(pairs))
 		copy(pairscopy, pairs)
 		ps := append(pairscopy[:i], pairscopy[i+1:]...)
 

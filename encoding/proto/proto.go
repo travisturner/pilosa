9e475d14723d29170d685ddd6b207a6c01ec709d
Travis Turner <travis@pilosa.com>
2018-09-05 10:55:00 -0500

    change Rows() to RowIDs() and add RowIdentifiers return type to hold row keys

--- encoding/proto/proto.go
+++ encoding/proto/proto.go
@@ -365,6 +365,9 @@ func encodeQueryResponse(m *pilosa.QueryResponse) *internal.QueryResponse {
 		case []pilosa.GroupCount:
 			pb.Results[i].Type = queryResultTypeGroupCounts
 			pb.Results[i].GroupCounts = encodeGroupCounts(result)
+		case pilosa.RowIdentifiers:
+			pb.Results[i].Type = queryResultTypeRowIdentifiers
+			pb.Results[i].RowIdentifiers = encodeRowIdentifiers(result)
 		case nil:
 			pb.Results[i].Type = queryResultTypeNil
 		}
@@ -838,7 +841,6 @@ func decodeQueryResponse(pb *internal.QueryResponse, m *pilosa.QueryResponse) {
 	}
 	m.Results = make([]interface{}, len(pb.Results))
 	decodeQueryResults(pb.Results, m.Results)
-
 }
 
 func decodeColumnAttrSets(pb []*internal.ColumnAttrSet, m []*pilosa.ColumnAttrSet) {
@@ -870,6 +872,7 @@ const (
 	queryResultTypeBool
 	queryResultTypeRowIDs
 	queryResultTypeGroupCounts
+	queryResultTypeRowIdentifiers
 )
 
 func decodeQueryResult(pb *internal.QueryResult) interface{} {
@@ -1006,6 +1009,14 @@ func encodeRow(r *pilosa.Row) *internal.Row {
 		Columns: r.Columns(),
 		Keys:    r.Keys,
 		Attrs:   encodeAttrs(r.Attrs),
+	}
+}
+
+func encodeRowIdentifiers(r pilosa.RowIdentifiers) *internal.RowIdentifiers {
+	return &internal.RowIdentifiers{
+		Rows: r.Rows,
+		Keys: r.Keys,
+		//Attrs:   encodeAttrs(r.Attrs),
 	}
 }
 
